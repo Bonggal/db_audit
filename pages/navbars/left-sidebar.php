eb47858b0d695934c13414fd733c4b0dc21d06c3
Bonggal Baringin Siahaan <bonggal.siahaan@gmail.com>
2019-03-26 10:07:20 +0700

    9th commit - 26th March 2019

--- pages/navbars/left-sidebar.php
+++ pages/navbars/left-sidebar.php
@@ -68,7 +68,7 @@
                 </a>
                 <ul class="treeview-menu">
                     <li><a href="/TA2/DBAudit/pages/database-ddl/ddl-activity.php">DDL Activities</a></li>
-                    <li><a href="#">DDL Users</a></li>
+                    <li><a href="/TA2/DBAudit/pages/database-ddl/ddl-users.php">DDL Users</a></li>
                     <li><a href="#">DDL Types</a></li>
                     <li><a href="#">DDL Objects</a></li>
 
